1d9519066da07a03a8d8815d55947e646b08abb6
juhtf <86907413+juhtf@users.noreply.github.com>
2022-02-10 17:14:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function phone(){
+    window.location="phone.html";
+}
+
+function laptop(){
+    window.location="laptop.html";
+}
+
+function book(){
+    window.location="book.html";
+}
+
+function cushion(){
+    window.location="cushion.html";
+}
---
+++ phone.js
@@ -0,0 +1,40 @@
+img=" ";
+status=" ";
+
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="status:Detecting Objects";
+}
+
+function preload(){
+    img=loadImage('phone.jfif');
+}
+
+function draw(){
+    image(img,0,0,640,420);
+    fill("#FF0000");
+    text("phone",45,75);
+    noFill();
+    stroke("green");
+    rect(30,60,450,350);
+}
+
+function back(){
+    window.location="index.html"
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!!!");
+    status=true;
+    objectDetector.detect(img,gotResults);
+    }
+    
+    function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    }
